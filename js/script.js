3a5d88f04cfe08b6ed927b0d3f3c60fcac608621
Anthony Crespo <bignorm01@hotmail.com>
2019-03-01 00:20:57 -0500

    add toggle_search_bar function and hide search bar by default

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function toggle_search_bar() {
+  var search_bar = document.getElementsByClassName('search-bar')[0];
+  search_bar.style.display = search_bar.style.display == "flex" ? "none" : "flex";
+}
